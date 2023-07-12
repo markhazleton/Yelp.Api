0e6801ad8c4d54a51e7ef0b7f82d87fc195980d2
Mark Hazleton <mark.hazleton@controlorigins.com>
2023-07-12 09:10:09 -0500

    Update Packages Add Unit Test

---
+++ Yelp.Api.Domain.Test/Models/AutocompleteResponseTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Yelp.Api.Domain.Test.Models;
+
+[TestClass]
+public class AutocompleteResponseTests
+{
+    [TestMethod]
+    public void AutocompleteResponse_Expected()
+    {
+        // Arrange
+        var autocompleteResponse = new AutocompleteResponse();
+
+        // Act
+
+
+        // Assert
+        Assert.IsNotNull(autocompleteResponse);
+    }
+}
